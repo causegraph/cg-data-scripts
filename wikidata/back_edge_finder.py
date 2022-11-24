502682fa9f9fb8110bd196fbdee788d8ea87c6be
Jamie Cox <jamie.day.cox@gmail.com>
2022-11-24 11:19:45 -0700

    show proportion of back edges to suggest where issues lie

--- wikidata/back_edge_finder.py
+++ wikidata/back_edge_finder.py
@@ -19,14 +19,18 @@ with open(date_path) as date_file, open(rel_path) as rel_file:
 
     back_edges = []
     back_edge_ctr = Counter()
+    rel_ctr = Counter()
+    proportions = dict()
 
     for rel in rels:
         src, type, dest = rel.split()
         if type not in excluded_rels and src in dates and dest in dates:
+            rel_ctr.update([type])
             d0, d1 = dates[src], dates[dest]
             if d0 > d1 and d0 - d1 > threshold:
                 print(wd_url + src, d0, type, wd_url + dest, d1)
                 back_edge_ctr.update([type])
-    print(back_edge_ctr)
-
-
+    print('back edge counts:', back_edge_ctr)
+    for type in back_edge_ctr:
+        proportions[type] = back_edge_ctr[type] / rel_ctr[type]
+    print('proportions:', proportions)
