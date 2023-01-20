9b65829326ade721321f8ccdbc5da99aaf0b75ba
Jamie Cox <jamie.day.cox@gmail.com>
2023-01-20 00:06:02 -0700

    nineteenth 2023 update to year_seq_check

--- wikidata/year_seq_check.py
+++ wikidata/year_seq_check.py
@@ -650,9 +650,9 @@ year_seqs = {
     },
     'dr_congo': {
         'start_id': 'Q52195848',
-        'end_id': 'Q111512655',
+        'end_id': 'Q116259582',
         'start_year': 1997,
-        'end_year': 2022,
+        'end_year': current_year,
         'search_query': {'prefix': '', 'suffix': ' in the Democratic Republic of the Congo', 'lang': 'en'},
     },
     'animation': {
@@ -2435,7 +2435,7 @@ year_seqs = {
     },
 }
 
-seq_to_check = 'newfoundland_labrador'
+seq_to_check = 'dr_congo'
 start_id = year_seqs[seq_to_check]['start_id']
 end_id = year_seqs[seq_to_check]['end_id']
 start_year = year_seqs[seq_to_check]['start_year']
