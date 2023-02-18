6f0f6ed84b0cd961b83aeae46ee0e3d997d30f6f
Jamie Cox <jamie.day.cox@gmail.com>
2023-02-18 13:20:15 -0700

    47th 2023 update to year_seq_check

--- wikidata/year_seq_check.py
+++ wikidata/year_seq_check.py
@@ -1329,9 +1329,9 @@ year_seqs = {
     },
     'bvi': {
         'start_id': 'Q43082320',
-        'end_id': 'Q110613262',
+        'end_id': 'Q116177949',
         'start_year': 2014,
-        'end_year': 2022,
+        'end_year': current_year,
         'search_query': {'prefix': '', 'suffix': ' in the British Virgin Islands', 'lang': 'en'},
     },
     'tonga': {
@@ -2435,7 +2435,7 @@ year_seqs = {
     },
 }
 
-seq_to_check = 'libya'
+seq_to_check = 'bvi'
 start_id = year_seqs[seq_to_check]['start_id']
 end_id = year_seqs[seq_to_check]['end_id']
 start_year = year_seqs[seq_to_check]['start_year']
