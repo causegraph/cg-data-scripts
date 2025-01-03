1f1d567ecd1ea4263d78666a931e586deb987fff
Jamie Cox <jamie.day.cox@gmail.com>
2025-01-03 08:38:37 -0700

    more year_seq_check updates

--- wikidata/year_seq_check.py
+++ wikidata/year_seq_check.py
@@ -31,7 +31,7 @@ current_year = 2025
 
 year_seqs = json.loads(open('years.json').read())
 
-seq_to_check = 'mykolaiv'
+seq_to_check = 'religion'
 
 start_id = year_seqs[seq_to_check]['start_id']
 end_id = year_seqs[seq_to_check]['end_id']
