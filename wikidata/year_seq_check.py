32f5cb65add8d42919ee1a0ee63a825e90bfb4fc
Jamie Cox <jamie.day.cox@gmail.com>
2025-05-25 10:40:02 -0600

    new year sequence

--- wikidata/year_seq_check.py
+++ wikidata/year_seq_check.py
@@ -31,7 +31,7 @@ current_year = 2025
 
 year_seqs = json.loads(open('years_auto.json').read())
 
-seq_to_check = 'ternopil_oblast2'
+seq_to_check = 'france2'
 
 start_id = year_seqs[seq_to_check]['start_id']
 end_id = year_seqs[seq_to_check]['end_id']
